d6ab66b0034b583083f23d59e3bb478ce31b6322
peterlucian <nasgaming92@gmail.com>
2025-05-01 07:58:08 +0100

    db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,25 +17,25 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@example.com',
-            'password'=> Hash::make('password'),
+            'name' => 'Pedro',
+            'email' => 'nasgaming92@gmail.com',
+            'password'=> Hash::make('92deP12E'),
             'role'=> 'admin',
         ]);
 
-        User::factory()->create([
-            'name' => 'Agent',
-            'email' => 'agent@example.com',
-            'password'=> Hash::make('password'),
-            'role'=> 'agent',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Agent',
+        //     'email' => 'agent@example.com',
+        //     'password'=> Hash::make('password'),
+        //     'role'=> 'agent',
+        // ]);
 
-        User::factory()->create([
-            'name' => 'User',
-            'email' => 'user@example.com',
-            'password'=> Hash::make('password'),
-            'role'=> 'user',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'User',
+        //     'email' => 'user@example.com',
+        //     'password'=> Hash::make('password'),
+        //     'role'=> 'user',
+        // ]);
 
         //$this->call(ItemSeeder::class);
         //$this->call(ImagePathSeeder::class);
